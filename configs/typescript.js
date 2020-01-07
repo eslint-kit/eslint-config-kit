0ef0c4d2243a0cd0783afbeb0e6e934b9bacbca4
risenforces <to@risen.dev>
2020-01-07 17:55:12 +0500

    finish ts config

--- configs/typescript.js
+++ configs/typescript.js
@@ -16,4 +16,86 @@ module.exports = {
       '@typescript-eslint/parser': extensions.tsExclusive,
     },
   },
+  rules: {
+    '@typescript-eslint/adjacent-overload-signatures': 'warn',
+    '@typescript-eslint/array-type': ['warn', { default: 'array-simple' }],
+    '@typescript-eslint/ban-ts-ignore': 'error',
+    '@typescript-eslint/ban-types': 'error',
+    'camelcase': 'off',
+    '@typescript-eslint/camelcase': ['error', { properties: 'always' }],
+    '@typescript-eslint/class-name-casing': 'error',
+    '@typescript-eslint/consistent-type-assertions': 'warn',
+    '@typescript-eslint/consistent-type-definitions': ['warn', 'interface'],
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/member-delimiter-style': [
+      'warn',
+      {
+        multiline: {
+          delimiter: 'comma',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: false,
+        },
+      },
+    ],
+    'no-array-constructor': 'off',
+    '@typescript-eslint/no-array-constructor': 'warn',
+    '@typescript-eslint/no-empty-interface': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-extra-non-null-assertion': 'warn',
+    '@typescript-eslint/no-for-in-array': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'error',
+    '@typescript-eslint/no-misused-new': 'error',
+    '@typescript-eslint/no-namespace': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'error',
+    '@typescript-eslint/no-unnecessary-condition': 'warn',
+    'no-unused-expressions': 'off',
+    '@typescript-eslint/no-unused-expressions': 'warn',
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'warn',
+    '@typescript-eslint/prefer-for-of': 'warn',
+    '@typescript-eslint/prefer-includes': 'warn',
+    '@typescript-eslint/prefer-namespace-keyword': 'error',
+    '@typescript-eslint/prefer-optional-chain': 'warn',
+    '@typescript-eslint/prefer-string-starts-ends-with': 'warn',
+    '@typescript-eslint/require-array-sort-compare': 'error',
+    'no-return-await': 'off',
+    '@typescript-eslint/return-await': ['error', 'in-try-catch'],
+    '@typescript-eslint/triple-slash-reference': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        //Checked by Typescript - ts(2378)
+        'getter-return': 'off',
+        // Checked by Typescript - ts(2300)
+        'no-dupe-args': 'off',
+        // Checked by Typescript - ts(1117)
+        'no-dupe-keys': 'off',
+        // Checked by Typescript - ts(7027)
+        'no-unreachable': 'off',
+        // Checked by Typescript - ts(2367)
+        'valid-typeof': 'off',
+        // Checked by Typescript - ts(2588)
+        'no-const-assign': 'off',
+        // Checked by Typescript - ts(2588)
+        'no-new-symbol': 'off',
+        // Checked by Typescript - ts(2376)
+        'no-this-before-super': 'off',
+        // This is checked by Typescript using the option `strictNullChecks`.
+        'no-undef': 'off',
+        // This is already checked by Typescript.
+        'no-dupe-class-members': 'off',
+        // This is already checked by Typescript.
+        'no-redeclare': 'off',
+      },
+    },
+  ],
 }
