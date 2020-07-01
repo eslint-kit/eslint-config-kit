2db8c32f1f647ad3a2bb7d2cf56634bb749bc873
risenforces <to@risen.dev>
2020-07-02 01:38:03 +0500

    fix tests

--- tests/requires.test.js
+++ tests/requires.test.js
@@ -2,6 +2,8 @@ it('should be successfully imported using require()', () => {
   const names = ['base', 'react', 'prettier', 'typescript', 'node']
 
   for (let name of names) {
-    expect(() => require('../npm/@eslint-kit/' + name)).not.toThrow()
+    expect(() =>
+      require('../npm/@eslint-kit/eslint-config-' + name),
+    ).not.toThrow()
   }
 })
